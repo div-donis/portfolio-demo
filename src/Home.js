4b51e86e1c891334f4850e7bf86ca27f787a14c5
div-donis <divdonis@gmail.com>
2022-01-12 14:44:37 -0800

    svg added

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Blog from "./Blog";
+import Avi from "./Avi";
 
 const Home = () => {
   return (
     <div className="home">
       <Blog />
+      <Avi />
     </div>
   );
 };
