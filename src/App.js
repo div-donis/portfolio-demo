87734c254cdd69691da7d264aefaebd07f864fcb
div-donis <divdonis@gmail.com>
2022-01-29 11:03:10 -0800

    draft 1 completed ready to deploy

--- src/App.js
+++ src/App.js
@@ -89,7 +89,30 @@ const App = () => {
 export const About = () => {
   return (
     <div className="about">
-      This is the page where you put details about yourself
+      <div className="about-container">
+        <h1>Portrait</h1>
+        <p>
+          Thank you for visiting my portfolio! My name is Donovan, or Don- I’m a
+          FullStack web developer that specializes in React, JavaScript & Redux
+          frontend & Ruby/RoR backend. I enjoy organizing ways to make web
+          development for efficient, coordinating innovative & communicative
+          ways to meet client arrangements & most of all, building projects and
+          implementing new technologies to challenge myself as a developer.
+          <br />
+          <br />
+          As a future Alumni of Flatiron school for Computer Software
+          Engineering (Graduating Feb 2022), I am looking forward to advancing
+          my career and implementing my skills the tech industry, as well as
+          furthering my education in the field & being a resource for others who
+          have a passion for tech.
+          <br />
+          <br />
+          I’ve worked as a freelance audio-engineer, producer & artist for the
+          past 10 years and in various Medical Laboratory roles for the past 7
+          years beginning in St. louis, MO and now in Seattle, WA. To contact
+          me, Please email <div id="email">donovan-odom@outlook.com</div>.
+        </p>
+      </div>
     </div>
   );
 };
